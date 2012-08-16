068de374e1d983cc9c2c164b5346b354a4b815b0
George Nash <georgen@quicinc.com>
2012-08-16 15:55:22 -0700

    Add constant values to Message.cs The unity bindings did not include the constants like MAX_NAME_LEN,  ALLJOYN_FLAG_COMMPRESSED, etc.  This adds those constants to the binding.

--- src/Message.cs
+++ src/Message.cs
@@ -20,24 +20,58 @@
  ******************************************************************************/
 
 using System;
+using System.Text;
 using System.Runtime.InteropServices;
 
 namespace AllJoynUnity
 {
 	public partial class AllJoyn
 	{
+		public const int ALLJOYN_MAX_NAME_LEN = 255;  /*!<  The maximum length of certain bus names */
+		public const int ALLJOYN_MAX_ARRAY_LEN = 131072;  /*!<  DBus limits array length to 2^26. AllJoyn limits it to 2^17 */
+		public const int ALLJOYN_MAX_PACKET_LEN = (ALLJOYN_MAX_ARRAY_LEN + 4096);  /*!<  DBus limits packet length to 2^27. AllJoyn limits it further to 2^17 + 4096 to allow for 2^17 payload */
+
+		/** @name Endianness indicators */
+		// @{
+		/** indicates the bus is little endian */
+		public const byte ALLJOYN_LITTLE_ENDIAN = 0x6c; // hex value for ascii letter 'l'
+		/** indicates the bus is big endian */
+		public const byte ALLJOYN_BIG_ENDIAN = 0x42; // hex value for ascii letter 'B'
+		// @}
+
+
+		/** @name Flag types */
+		// @{
+		/** No reply is expected*/
+		public const byte ALLJOYN_FLAG_NO_REPLY_EXPECTED = 0x01;
+		/** Auto start the service */
+		public const byte ALLJOYN_FLAG_AUTO_START = 0x02;
+		/** Allow messages from remote hosts (valid only in Hello message) */
+		public const byte ALLJOYN_FLAG_ALLOW_REMOTE_MSG = 0x04;
+		/** Global (bus-to-bus) broadcast */
+		public const byte ALLJOYN_FLAG_GLOBAL_BROADCAST = 0x20;
+		/** Header is compressed */
+		public const byte ALLJOYN_FLAG_COMPRESSED = 0x40;
+		/** Body is encrypted */
+		public const byte ALLJOYN_FLAG_ENCRYPTED = 0x80;
+		// @}
+
+		/** ALLJOYN protocol version */
+		public const byte ALLJOYN_MAJOR_PROTOCOL_VERSION = 1;
+
 		/**
 		 * Message is a reference counted (managed) version of _Message
 		 */
 		public class Message : IDisposable
 		{
+			/** Message types */
 			public enum Type : int
 			{
-				Invalid = 0,
-				MethodCall = 1,
-				MethodReturn = 2,
-				Error = 3,
-				Signal = 4
+				Invalid = 0, ///< an invalid message type
+				MethodCall = 1, ///< a method call message type
+				MethodReturn = 2, ///< a method return message type
+				Error = 3, ///< an error message type
+				Signal = 4 ///< a signal message type
 			}
 
 			    /**
@@ -64,14 +98,14 @@ namespace AllJoynUnity
 			}
 
 			/**
-			     * Return a specific argument.
-			     *
-			     * @param index  The index of the argument to get.
-			     *
-			     * @return
-			     *      - The argument
-			     *      - NULL if unmarshal failed or there is not such argument.
-			     */
+			 * Return a specific argument.
+			 *
+			 * @param index  The index of the argument to get.
+			 *
+			 * @return
+			 *      - The argument
+			 *      - NULL if unmarshal failed or there is not such argument.
+			 */
 			public MsgArg GetArg(int index)
 			{
 			
@@ -80,18 +114,27 @@ namespace AllJoynUnity
 			}
 
 			/**
-			     * Accessor function to get the sender for this message.
-			     *
-			     * @return
-			     *      - The senders well-known name string stored in the AllJoyn header field.
-			     *      - An empty string if the message did not specify a sender.
-			     */
+			 * Accessor function to get the sender for this message.
+			 *
+			 * @return
+			 *      - The senders well-known name string stored in the AllJoyn header field.
+			 *      - An empty string if the message did not specify a sender.
+			 */
 		    public string GetSender()
 		    {
 			IntPtr sender = alljoyn_message_getsender(_message);
 			return (sender != IntPtr.Zero ? Marshal.PtrToStringAnsi(sender) : null);
 		    }
 
+			/**
+			 * Return a specific argument.
+			 *
+			 * @param index  The index of the argument to get.
+			 *
+			 * @return
+			 *      - The argument
+			 *      - NULL if unmarshal failed or there is not such argument.
+			 */
 			public MsgArg this[int i]
 			{
 				get
